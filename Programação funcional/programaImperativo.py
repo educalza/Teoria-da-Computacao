8392ed280e6c88e51e474decc053ebb6a5c57823
Eduardo <eduardoalvescalza20@gmail.com>
2024-12-20 22:50:15 -0300

    changes

--- Programação funcional/programaImperativo.py
+++
@@ -1,9 +0,0 @@
-def calcular_media_imperativa(numeros):
-    soma = 0
-    for numero in numeros:
-        soma += numero
-    return soma / len(numeros)
-
-numeros = [1, 2, 3, 4, 5]
-media = calcular_media_imperativa(numeros)
-print(media)
